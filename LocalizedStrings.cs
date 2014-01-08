17b8bb28a410fbdd9b2102c5f1e8da7967d040e7
FindingNamo <davidminhnamngo@gmail.com>
2014-01-07 23:39:05 -0800

    Round 2 Fight

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Hearthopedia.Resources;
+
+namespace Hearthopedia
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
